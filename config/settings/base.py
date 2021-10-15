117490017c9628af34cc39c36ec5d31745e1fac2
Pratap <pratap.kute97@gmail.com>
2021-10-15 08:59:04 +0530

    store app added in local app list

--- config/settings/base.py
+++ config/settings/base.py
@@ -74,6 +74,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "e_commerce.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "e_commerce.stores.apps.StoresConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
