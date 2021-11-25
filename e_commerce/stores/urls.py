c411c3c2dc9a4903b75a97338253425944f7a0db
Pratap Kute <pratap.kute97@gmail.com>
2021-11-25 17:43:17 +0530

    urls for details and category

--- e_commerce/stores/urls.py
+++ e_commerce/stores/urls.py
@@ -5,5 +5,6 @@ from e_commerce.stores.views import *
 app_name = "stores"
 urlpatterns = [
     path('', all_products, name='all_products'),
-    path('item/<slug:slug>/', product_detail, name='product_detail')
+    path('item/<slug:slug>/', product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', category_list, name='category_list')
 ]
