40ccf0425d7b717bbd7d8aeff7a259b8dbb892ab
Pratap Kute <pratap.kute97@gmail.com>
2021-11-26 14:41:16 +0530

    pytest correction

--- e_commerce/stores/tests/test_models.py
+++ e_commerce/stores/tests/test_models.py
@@ -1,48 +1,47 @@
 import pytest
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from e_commerce.stores.models import Category, Product
-from e_commerce.users.models import User
 
 pytestmark = pytest.mark.django_db
 
 
-class TestCategories(TestCase):
-    def setUp(self) -> None:
+class TestCategoriesModel(TestCase):
+    def setUp(self):
         self.data1 = Category.objects.create(name="django", slug="django")
 
     def test_category_model_entry(self):
         """
-        Test Category model data insertion/type/field attribute
+        Test Category model data insertion/types/field attributes
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
-
-    def test_category_model_entry_default_name(self):
-        """
-        Test Category model default name
-        :return: True or False
-        """
-        data = self.data1
         self.assertEqual(str(data), "django")
 
 
 class TestProductsModel(TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
+        get_user_model().objects.create_user(
+            username="test", password="12test12", email="test@example.com"
+        )
+        # User.objects.create(username='admin')
         Category.objects.create(name="django", slug="django")
-        User.objects.create(username="admin")
+        id_dj = Category.objects.only("id").get(name="django").id
         self.data1 = Product.objects.create(
-            category_id=1,
+            category_id=id_dj,
             title="django beginners",
             created_by_id=1,
             slug="django-beginners",
             price="20.00",
             image="django",
         )
+        # self.data2 = Product.products.create(category_id=1, title='django advanced', created_by_id=1,
+        #                                      slug='django-advanced', price='20.00', image='django', is_active=False)
 
     def test_products_model_entry(self):
         """
-        Test product model data insertion/type/field attributes
+        Test product model data insertion/types/field attributes
         """
         data = self.data1
         self.assertTrue(isinstance(data, Product))
