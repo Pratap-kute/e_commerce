4dba04896cd738fb3b491537e8e85022ec4f4641
Pratap Kute <pratap.kute97@gmail.com>
2021-11-29 14:23:47 +0530

    url naming improved

--- e_commerce/stores/urls.py
+++ e_commerce/stores/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from e_commerce.stores.views import all_products, category_list, product_detail
+from e_commerce.stores.views import category_list, product_all, product_detail
 
 app_name = "stores"
 urlpatterns = [
-    path("", all_products, name="all_products"),
-    path("item/<slug:slug>/", product_detail, name="product_detail"),
+    path("", product_all, name="product_all"),
+    path("<slug:slug>", product_detail, name="product_detail"),
     path("search/<slug:category_slug>/", category_list, name="category_list"),
 ]
