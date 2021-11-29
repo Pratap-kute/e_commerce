2783102cc0048fac2eca464c277540aa6e2e32cd
Pratap Kute <pratap.kute97@gmail.com>
2021-11-29 14:25:06 +0530

    model manager added to get only active stock list (study more on this)

--- e_commerce/stores/models.py
+++ e_commerce/stores/models.py
@@ -4,6 +4,11 @@ from django.urls import reverse
 from e_commerce.users.models import User
 
 
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -37,6 +42,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = "products"
