640b2afe9a83b7149e1dad68303aa820cdf16eca
Pratap Kute <pratap.kute97@gmail.com>
2021-11-30 17:38:45 +0530

    improper url redirect fix

--- e_commerce/stores/urls.py
+++ e_commerce/stores/urls.py
@@ -5,6 +5,6 @@ from e_commerce.stores.views import category_list, product_all, product_detail
 app_name = "stores"
 urlpatterns = [
     path("", product_all, name="product_all"),
-    path("<slug:slug>", product_detail, name="product_detail"),
+    path("item/<slug:slug>", product_detail, name="product_detail"),
     path("shop/<slug:category_slug>/", category_list, name="category_list"),
 ]
